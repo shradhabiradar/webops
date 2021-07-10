dfe31b18b96b43abd3d5518d60a38a6a8fa92dbc
shradha biradar <76563093+shradhabiradar@users.noreply.github.com>
2021-07-10 19:31:32 +0530

    Update urlshortner.js

--- urlshortner/model/urlshortner.js
+++ urlshortner/model/urlshortner.js
@@ -1,7 +1,11 @@
+// requiring mongoose 
 const mongoose = require('mongoose');
+//  used to create short non-sequential url-friendly unique ids
 const shortId = require('shortid');
 
+// creating a schema, helps in validation
 const urlSchema = new mongoose.Schema({
+    //store the url which we get from the user
     full:{
         type: String,
         required: true,
@@ -9,9 +13,11 @@ const urlSchema = new mongoose.Schema({
     short: {
         type: String,
         required: true,
+//         it generates random id
         default: shortId.generate
     }
 },{
+    //to matain the time
     timestamps: true
 });
 
