f8778bc645dba3706a8df7d1f4020cf001b48558
shradha biradar <76563093+shradhabiradar@users.noreply.github.com>
2021-07-10 19:25:54 +0530

    Update mongoose.js

--- urlshortner/config/mongoose.js
+++ urlshortner/config/mongoose.js
@@ -1,12 +1,18 @@
+// we have installed mongoose by npm, now we have to use this package so we 'require'
+
 const mongoose = require('mongoose');
+
+// connect to the local host
 mongoose.connect('mongodb://localhost/url_shortner');
+// mongoose is exposed and have nly one DB connection
 const db = mongoose.connection;
 
+// check if the mongoose has connected successfully
 db.on('error', console.error.bind(console, 'error in connecting the mongodb'));
 
 db.once('open', function(){
     console.log('mongodb connected successfully');
 });
 
-
+// exporting module so that it is available outsite this module
 module.exports = db;
